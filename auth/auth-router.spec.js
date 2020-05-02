8b97d1200559fad29077e875a88edfff0ba8f042
Jackie Atwood <takalaj@verizon.net>
2020-05-01 23:06:51 -0400

    MVP

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -37,4 +37,30 @@ describe("auth-router.js", () => {
             })
         })
     })
+
+    describe("/login endpoint", () => {
+        // beforeEach(async () => {
+        //     await db("users").truncate();
+        // })
+        
+        const userInfo = {username: "JaxSprint3", password: "password3"};
+
+        it("Should return a 200", () => {
+            let response 
+            return request(server).post("/api/auth/login").send(userInfo).then(res => {
+                response = res
+                expect(response.status).toBe(200);
+            })
+        })
+
+        it("Should return login message", () => {
+            const message = "Welcome JaxSprint3";
+
+            let response
+            return request(server).post("/api/auth/login").send(userInfo).then(res => {
+                response = res
+                expect(response.body.message).toMatch(message);
+            })
+        })
+    })
 })
