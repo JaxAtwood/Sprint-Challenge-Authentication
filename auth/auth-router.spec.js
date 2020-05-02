25ad11272f6cc1a231680bc5319921c28f1e1fa2
Jackie Atwood <takalaj@verizon.net>
2020-05-01 22:29:31 -0400

    Server.spec.js

---
+++ auth/auth-router.spec.js
@@ -0,0 +1,13 @@
+const supertest = require("supertest");
+const server = require("../api/server.js");
+const db = require("../database/dbConfig.js");
+
+const Users = require("../users/users-model.js");
+
+describe("", () => {
+    
+
+    describe("", () => {
+        
+    })
+})
